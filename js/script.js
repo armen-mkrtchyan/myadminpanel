d8e452544a09feba27160665c4b70e51e435793c
armen-mkrtchyan <armen-mkrtchyan93@mail.ru>
2019-12-19 15:51:45 +0400

    changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 startLoading();
-
-
 	$.ajax({
     type:'GET',
     url:'json/nav.json',
@@ -35,6 +33,111 @@ startLoading();
      }
 });
 
+$(document).ready(function() {
+    getLeftBtnData("json/left_btn.json", (data) => {
+        let container = document.getElementsByClassName('accordion_container')[0];
+        for(let i = 0; i < data.length; i++) {
+            let accordion_head = document.createElement('div');
+
+            let row = document.createElement('div');
+            row.style = "display:flex; align-items: center";
+
+            let text = document.createElement('span');
+
+
+
+            let img = document.createElement('img');
+            let img_minus = document.createElement('img');
+
+
+            text.innerHTML = data[i].headName;
+            img.src = data[i].icon;
+            img_minus.src = "img/plus.png";
+
+            img_minus.onclick = function() {
+                let id = data[i].id.toString();
+                if(img_minus.src.indexOf("plus.png") !== -1){
+                    img_minus.src = "img/minus.png";
+                    document.getElementById(id).style.display = "block";
+                }else{
+                    img_minus.src = "img/plus.png";
+                    document.getElementById(id).style.display = "none";
+                }
+            };
+
+            row.appendChild(img_minus);
+            row.appendChild(img);
+            row.appendChild(text);
+            accordion_head.appendChild(row);
+
+            accordion_head.style.paddingLeft = data[i].padding_left;
+            let localData = data[i];
+            while(localData.subMenu){
+                let accordion_head_inner = document.createElement('div');
+                accordion_head_inner.id = localData.id;
+                for(let j = 0; j < localData.subMenu.length; j++) {
+                    let row_inner = document.createElement('div');
+                    row_inner.style = "display:flex; align-items: center";
+
+                    let text_inner = document.createElement('span');
+                    let img_inner = document.createElement('img');
+                    let img_minus_inner = document.createElement('img');
+
+                    text_inner.innerHTML = localData.subMenu[j].headName;
+                    img_inner.src = localData.subMenu[j].icon;
+                    img_minus_inner.src = "img/plus.png";
+
+                    let id = localData.subMenu[j].id.toString();
+                    img_minus_inner.onclick = function() {
+                        if(img_minus_inner.src.indexOf("plus.png") !== -1){
+                            img_minus_inner.src = "img/minus.png";
+                            //document.getElementById(id).style.display = "block";
+                        }else{
+                            img_minus_inner.src = "img/plus.png";
+                            //document.getElementById(id).style.display = "none";
+                        }
+                    };
+
+                    row_inner.appendChild(img_minus_inner);
+                    row_inner.appendChild(img_inner);
+                    row_inner.appendChild(text_inner);
+
+                    accordion_head_inner.appendChild(row_inner);
+
+                    accordion_head_inner.style.paddingLeft = localData.subMenu[j].padding_left;
+                    accordion_head.appendChild(accordion_head_inner);
+                    accordion_head_inner.style.display = "none";
+                }
+                localData = localData.subMenu;
+            }
+            container.appendChild(accordion_head);
+        }
+    });
+
+
+
+
+// <div class="accordion_head">
+//         <span class="plusminus"></span>
+//         <img src="img/plus.png" alt="">
+//         </div>
+
+    //toggle the component with class accordion_body
+    // $(".accordion_head").click(function() {
+    //     if ($('.accordion_body').is(':visible')) {
+    //         $(".accordion_body").slideUp(300);
+    //         $(".plusminus").text('+');
+    //     }
+    //     if ($(this).next(".accordion_body").is(':visible')) {
+    //         $(this).next(".accordion_body").slideUp(300);
+    //         $(this).children(".plusminus").text('+');
+    //     } else {
+    //         $(this).next(".accordion_body").slideDown(300);
+    //         $(this).children(".plusminus").text('-');
+    //     }
+    // });
+    //
+});
 
 
 
@@ -42,7 +145,7 @@ startLoading();
 
 setTimeout(()=>{	
 	removeLoading("loading");
-}, 200)
+}, 600)
 
 
 
@@ -55,8 +158,8 @@ function startLoading(){
 					width : 100%; 
 					background-color:rgba(0,0,0,0.6);
 					position: absolute;
-					top : 0;
-					left : 0;
+					top : auto;
+					left : auto;
 					
 					`;
 	document.getElementsByTagName("body")[0].appendChild(element);
@@ -67,4 +170,15 @@ function removeLoading(elementId){
 	if(removeElement){
 		removeElement.parentNode.removeChild(removeElement);
 	}
+}
+
+
+function getLeftBtnData(url, cb){
+    $.ajax({
+        type:'GET',
+        url:url,
+        dataType:'json',
+        success: function(data) {
+            cb(data);
+        }});
 }
